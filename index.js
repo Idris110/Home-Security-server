a486e39807cb5dcc4bfc8104e979ffcaeba554ca
Idris110 <ratlamwalaidris110@gmail.com>
2023-02-18 03:36:36 +0530

    prob

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require("cors");
 
-// const fetch = require('node-fetch');
-// const Joi = require('joi'); //used for validation
 const app = express();
 
 app.use(
