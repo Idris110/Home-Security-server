6dbbf9ebb57474704c505680915209b453c0891e
Idris110 <ratlamwalaidris110@gmail.com>
2023-02-17 12:24:32 +0530

    port

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.post('/api/set', (req, res) => {
 });
 
 //PORT ENVIRONMENT VARIABLE debug : heroku logs -t --app esp8266-api-testing
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
